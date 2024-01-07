655a36d85cbb4360523c254f54c86a46f3d82131
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-01-07 21:04:38 +0530

    commit5

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-//Refer diary
 const config = require('./utils/config');
-//use mongoose
-const mongoose = require ('mongoose');
-//import app here
+const mongoose = require('mongoose');
 const app = require('./app');
 
-//mongodb connection
-console.log('connecting to MongoDB');
-mongoose.connect(config.MONGO_URI)
-.then(() => {
+// MongoDB connection
+console.log('Connecting to MongoDB');
+mongoose
+  .connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(config.PORT,()=>{
-        console.log(`server is running on port ${config.PORT}`);
+    
+    // Start Express app after successful MongoDB connection
+    app.listen(config.PORT, () => {
+      console.log(`Server is running on port ${config.PORT}`);
     });
   })
   .catch((error) => {
